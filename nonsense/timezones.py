5bdbbd8a84c276c5e497ed0a135778d49f0c6cb1
Christopher Hoppa <64328096+ChristopherHoppa@users.noreply.github.com>
2023-09-07 09:46:56 -0400

    Update timezones.py
    acronymns

--- nonsense/timezones.py
+++ nonsense/timezones.py
@@ -1,52 +1,53 @@
-us_state_timezones = {
-    'AL': 'Central Time Zone',
-    'AK': 'Alaska Time Zone',
-    'AZ': 'Mountain Time Zone',
-    'AR': 'Central Time Zone',
-    'CA': 'Pacific Time Zone',
-    'CO': 'Mountain Time Zone',
-    'CT': 'Eastern Time Zone',
-    'DE': 'Eastern Time Zone',
-    'FL': 'Eastern Time Zone',
-    'GA': 'Eastern Time Zone',
-    'HI': 'Hawaii-Aleutian Time Zone',
-    'ID': 'Mountain Time Zone',
-    'IL': 'Central Time Zone',
-    'IN': 'Eastern Time Zone',
-    'IA': 'Central Time Zone',
-    'KS': 'Central Time Zone',
-    'KY': 'Eastern Time Zone',
-    'LA': 'Central Time Zone',
-    'ME': 'Eastern Time Zone',
-    'MD': 'Eastern Time Zone',
-    'MA': 'Eastern Time Zone',
-    'MI': 'Eastern Time Zone',
-    'MN': 'Central Time Zone',
-    'MS': 'Central Time Zone',
-    'MO': 'Central Time Zone',
-    'MT': 'Mountain Time Zone',
-    'NE': 'Central Time Zone',
-    'NV': 'Pacific Time Zone',
-    'NH': 'Eastern Time Zone',
-    'NJ': 'Eastern Time Zone',
-    'NM': 'Mountain Time Zone',
-    'NY': 'Eastern Time Zone',
-    'NC': 'Eastern Time Zone',
-    'ND': 'Central Time Zone',
-    'OH': 'Eastern Time Zone',
-    'OK': 'Central Time Zone',
-    'OR': 'Pacific Time Zone',
-    'PA': 'Eastern Time Zone',
-    'RI': 'Eastern Time Zone',
-    'SC': 'Eastern Time Zone',
-    'SD': 'Central Time Zone',
-    'TN': 'Central Time Zone',
-    'TX': 'Central Time Zone',
-    'UT': 'Mountain Time Zone',
-    'VT': 'Eastern Time Zone',
-    'VA': 'Eastern Time Zone',
-    'WA': 'Pacific Time Zone',
-    'WV': 'Eastern Time Zone',
-    'WI': 'Central Time Zone',
-    'WY': 'Mountain Time Zone'
+state_time_zones = {
+    'AL': 'CST',  # Alabama - Central Standard Time
+    'AK': 'AKST', # Alaska - Alaska Standard Time
+    'AZ': 'MST',  # Arizona - Mountain Standard Time
+    'AR': 'CST',  # Arkansas - Central Standard Time
+    'CA': 'PST',  # California - Pacific Standard Time
+    'CO': 'MST',  # Colorado - Mountain Standard Time
+    'CT': 'EST',  # Connecticut - Eastern Standard Time
+    'DE': 'EST',  # Delaware - Eastern Standard Time
+    'FL': 'EST',  # Florida - Eastern Standard Time
+    'GA': 'EST',  # Georgia - Eastern Standard Time
+    'HI': 'HST',  # Hawaii - Hawaii Standard Time
+    'ID': 'MST',  # Idaho - Mountain Standard Time
+    'IL': 'CST',  # Illinois - Central Standard Time
+    'IN': 'EST',  # Indiana - Eastern Standard Time
+    'IA': 'CST',  # Iowa - Central Standard Time
+    'KS': 'CST',  # Kansas - Central Standard Time
+    'KY': 'EST',  # Kentucky - Eastern Standard Time
+    'LA': 'CST',  # Louisiana - Central Standard Time
+    'ME': 'EST',  # Maine - Eastern Standard Time
+    'MD': 'EST',  # Maryland - Eastern Standard Time
+    'MA': 'EST',  # Massachusetts - Eastern Standard Time
+    'MI': 'EST',  # Michigan - Eastern Standard Time
+    'MN': 'CST',  # Minnesota - Central Standard Time
+    'MS': 'CST',  # Mississippi - Central Standard Time
+    'MO': 'CST',  # Missouri - Central Standard Time
+    'MT': 'MST',  # Montana - Mountain Standard Time
+    'NE': 'CST',  # Nebraska - Central Standard Time
+    'NV': 'PST',  # Nevada - Pacific Standard Time
+    'NH': 'EST',  # New Hampshire - Eastern Standard Time
+    'NJ': 'EST',  # New Jersey - Eastern Standard Time
+    'NM': 'MST',  # New Mexico - Mountain Standard Time
+    'NY': 'EST',  # New York - Eastern Standard Time
+    'NC': 'EST',  # North Carolina - Eastern Standard Time
+    'ND': 'CST',  # North Dakota - Central Standard Time
+    'OH': 'EST',  # Ohio - Eastern Standard Time
+    'OK': 'CST',  # Oklahoma - Central Standard Time
+    'OR': 'PST',  # Oregon - Pacific Standard Time
+    'PA': 'EST',  # Pennsylvania - Eastern Standard Time
+    'RI': 'EST',  # Rhode Island - Eastern Standard Time
+    'SC': 'EST',  # South Carolina - Eastern Standard Time
+    'SD': 'CST',  # South Dakota - Central Standard Time
+    'TN': 'CST',  # Tennessee - Central Standard Time
+    'TX': 'CST',  # Texas - Central Standard Time
+    'UT': 'MST',  # Utah - Mountain Standard Time
+    'VT': 'EST',  # Vermont - Eastern Standard Time
+    'VA': 'EST',  # Virginia - Eastern Standard Time
+    'WA': 'PST',  # Washington - Pacific Standard Time
+    'WV': 'EST',  # West Virginia - Eastern Standard Time
+    'WI': 'CST',  # Wisconsin - Central Standard Time
+    'WY': 'MST',  # Wyoming - Mountain Standard Time
 }
+
